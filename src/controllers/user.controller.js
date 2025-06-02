281fb0509c3694aa121804f18dae8c6f88ff2d11
it-instagrp <it.team.instaict@gmail.com>
2025-06-02 11:33:47 +0530

    Update: Updated User controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,1 +1,23 @@
 import HttpStatus from 'http-status-codes';
+import * as UserService from '../services/user.service.js';
+
+/**
+ * Controller to get all users available
+ * @param  {object} req - request object
+ * @param {object} res - response object
+ * @param {Function} next
+ */
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const data = await UserService.getAllUsers();
+    res.status(HttpStatus.OK).json({
+      code: HttpStatus.OK,
+      data: data,
+      message: 'All users fetched successfully',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**** Just like this add all controller methods ****/
